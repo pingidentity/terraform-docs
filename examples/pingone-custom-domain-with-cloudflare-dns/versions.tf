33d7624f7aa1eab3dd0531b45155ca576907cffd
Patrick Cowland <patrickcowland@pingidentity.com>
2023-09-19 15:22:31 +0100

    update versions for cloudflare dns example

--- examples/pingone-custom-domain-with-cloudflare-dns/versions.tf
+++ examples/pingone-custom-domain-with-cloudflare-dns/versions.tf
@@ -2,21 +2,23 @@ terraform {
   required_version = ">= 1.2.0"
 
   required_providers {
-    pingone = {
-      source  = "pingidentity/pingone"
-      version = ">= 0.21.0, < 1.0.0"
-    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.4.0"
     }
+    pingone = {
+      source  = "pingidentity/pingone"
+      version = ">= 0.21.0, < 1.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.1, < 1.0.0"
+    }
   }
 }
 
-provider "cloudflare" {
-  # Configuration options
-}
+provider "cloudflare" {}
 
-provider "pingone" {
-  # Configuration options
-}
+provider "pingone" {}
+
+provider "time" {}
