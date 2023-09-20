9adfd614b004e56e19065fccfd43c6806abefdd7
Patrick Cowland <patrickcowland@pingidentity.com>
2023-09-20 09:06:46 +0100

    correct lint error

--- examples/sso-to-pingdirectory-from-pingone/main.tf
+++ examples/sso-to-pingdirectory-from-pingone/main.tf
@@ -6,7 +6,7 @@
 ###########################################
 
 locals {
-  pingdirectory_login_path = var.pingdirectory_ldap_host != null && var.pingdirectory_ldap_host != "" && var.pingdirectory_ldap_port != null ? format("?ldap-hostname=%s&ldaps-port=%s", var.pingdirectory_ldap_host, var.pingdirectory_ldap_port) : format("/login")
+  pingdirectory_login_path = var.pingdirectory_ldap_host != null && var.pingdirectory_ldap_host != "" && var.pingdirectory_ldaps_port != null ? format("?ldap-hostname=%s&ldaps-port=%s", var.pingdirectory_ldap_host, var.pingdirectory_ldaps_port) : format("/login")
 }
 
 # Create the environment
